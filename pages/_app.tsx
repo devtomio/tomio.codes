ebaea0616d2fa7d82374efb22d6d5c1e3f500934
Tomio <mail@tomio.codes>
2022-02-08 22:50:52 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,10 @@ const App = ({ Component, pageProps }: AppProps) => (
 
             <meta name="apple-mobile-web-app-capable" content="yes" />
 
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+
             <link
                 rel="apple-touch-startup-image"
                 href="/icons/apple-splash-2048-2732.jpg"
