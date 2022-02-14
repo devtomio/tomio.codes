5cd4314d9f2bd37cc75b142041acabca6a53350c
Tomio <mail@tomio.codes>
2022-02-14 09:04:21 +0800

    feat(fonts): change roboto to lora

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ const App = ({ Component, pageProps }: AppProps) => (
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link
-                href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Patrick+Hand&family=Roboto&display=swap"
+                href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Patrick+Hand&family=Lora&display=swap"
                 rel="stylesheet"
             />
 
