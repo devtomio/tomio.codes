56e6d1ec628102811afb98df053f9ebeecfeb0a4
Tomio <mail@tomio.codes>
2022-02-14 09:46:19 +0800

    fix: add center class

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,23 +72,6 @@ const Home = ({ data }: { data: Props }) => (
                             </p>
                         </div>
                     </section>
-                    <div>
-                        <a
-                            href="https://www.youtube.com/channel/UCzep0xXUWcZ6SukIS1UtLHw"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                style={{ fill: '#fff !important' }}
-                            >
-                                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
-                            </svg>
-                        </a>
-                    </div>
                     {/* <div className="buttons">
                         <a href="/discord" className="btn discord-dropshadow animation hover fira">
                             Discord
@@ -104,7 +87,7 @@ const Home = ({ data }: { data: Props }) => (
                         </a>
                             </div> */}
                 </div>
-                <div className="container">
+                <div className="container center">
                     <a href="https://www.youtube.com/channel/UCzep0xXUWcZ6SukIS1UtLHw" target="_blank" rel="noreferrer">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
